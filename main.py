f33045639671efe58b8a60ebb0e417366cf22a76
Daniel Chicot <dan.chicot@gmail.com>
2020-12-11 13:40:40 +0000

    Sync

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ import gzip
 
 
 def main():
-    blobs = {}
+    batches = {}
     counts = {}
     for line in fileinput.input():
         prefix = line[:3]
-        counts[prefix] = counts[prefix] + 1 if prefix in counts else 1
-        blobs[prefix] = blobs[prefix] if prefix in blobs else []
-        blobs[prefix].append(line.encode())
-        if len(blobs[prefix]) == 7:
-            output_file = f"partitioned/{prefix}-{counts[prefix]:06d}.txt.gz"
-            with gzip.open(output_file, 'wb') as f:
-                for guid in blobs[prefix]:
-                    f.write(guid)
-                print(f"Wrote {output_file}")
+        batches[prefix] = batches[prefix] if prefix in batches else []
+        batches[prefix].append(line.encode())
+        if len(batches[prefix]) > 1_000_000:
+            process_batch(batches, counts, prefix)
+
+    for prefix in batches:
+        process_batch(batches, counts, prefix)
+
+
+def process_batch(batches, counts, prefix):
+    counts[prefix] = counts[prefix] + 1 if prefix in counts else 1
+    write_batch(batches[prefix], counts[prefix], prefix)
+    batches[prefix] = []
+
+
+def write_batch(batch, count, prefix):
+    output_file = f"partitioned/{prefix}-{count:06d}.txt.gz"
+    with gzip.open(output_file, 'wb') as f:
+        for guid in batch:
+            f.write(guid)
+    print(f"Wrote batch {output_file}")
 
 
 if __name__ == '__main__':
