17a54944aaae5db3c9eab75c3ed2f3096fefce8d
Daniel Chicot <dan.chicot@gmail.com>
2020-12-11 11:14:27 +0000

    sync

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ def main():
         blobs[prefix] = blobs[prefix] if prefix in blobs else []
         blobs[prefix].append(line.encode())
         if len(blobs[prefix]) == 7:
-            output_file = f"{prefix}-{counts[prefix]:06d}.txt.gz"
+            output_file = f"partitioned/{prefix}-{counts[prefix]:06d}.txt.gz"
             with gzip.open(output_file, 'wb') as f:
                 for guid in blobs[prefix]:
                     f.write(guid)
                 print(f"Wrote {output_file}")
-
 
 
 if __name__ == '__main__':
