efe19cd56cc197b8eb70f4a46313a9acd6253c74
Daniel Chicot <dan.chicot@gmail.com>
2020-12-11 14:40:19 +0000

    sync

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import fileinput
 import gzip
@@ -11,8 +11,10 @@ def main():
         prefix = line[:3]
         batches[prefix] = batches[prefix] if prefix in batches else []
         batches[prefix].append(line.encode())
-        if len(batches[prefix]) > 1_000_000:
+        if len(batches[prefix]) > 10_000:
             process_batch(batches, counts, prefix)
+        elif len(batches[prefix]) % 1_000 == 0 and prefix == "aaa":
+            print(f"{prefix} size: {len(batches[prefix])}")
 
     for prefix in batches:
         process_batch(batches, counts, prefix)
@@ -27,8 +29,8 @@ def process_batch(batches, counts, prefix):
 def write_batch(batch, count, prefix):
     output_file = f"partitioned/{prefix}-{count:06d}.txt.gz"
     with gzip.open(output_file, 'wb') as f:
-        for guid in batch:
-            f.write(guid)
+        contents = b''.join(batch)
+        f.write(contents)
     print(f"Wrote batch {output_file}")
 
 
